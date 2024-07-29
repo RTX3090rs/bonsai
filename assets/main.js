04335f617aadce83649bdc2e5a0fd5f2b08dcb77
Gustavo Jorge <gustavojorgehz@gmail.com>
2024-07-28 21:39:18 -0300

    bonsai_v1
    Adicionei o projeto ao repositório

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+function clicar(){
+    const background_bt = document.querySelector('div#clicar')
+    background_bt.classList.toggle("background_after_click")
+    let price = document.querySelectorAll('div.price')
+}
